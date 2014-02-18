a9b088d29c0f8c020d50fe27c727c32a821237a0
hackerljm <hackerljm@gmail.com>
2014-02-18 09:13:32 +0000

    Fixed #0016: 크로스 사이트 스크립팅 방어 로직 적용.

---
+++ haengbokhan/src/main/java/org/haengbokhan/filter/xss/XSSFilter.java
@@ -0,0 +1,29 @@
+package org.haengbokhan.filter.xss;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.GenericFilterBean;
+
+@Component("xssFilter")
+public class XSSFilter extends GenericFilterBean {
+
+	/**
+	 * @see javax.servlet.Filter#doFilter(javax.servlet.ServletRequest,
+	 *      javax.servlet.ServletResponse, javax.servlet.FilterChain)
+	 */
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response,
+			FilterChain chain) throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		chain.doFilter(new XSSRequestWrapper((HttpServletRequest) request),
+				response);
+	}
+
+}
